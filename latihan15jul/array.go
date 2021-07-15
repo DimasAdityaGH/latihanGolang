fa8ad86ccf90e605c5a6cf60b5d2fba6d57135f5
dimasAditya <dimasubuntu21@gmail.com>
2021-07-15 14:33:36 +0700

    array

---
+++ latihan15jul/array.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var murid [9]string
+	murid[0] = "dimas,"
+	murid[1] = "dina,"
+	murid[2] = "agus,"
+	murid[3] = "dafa,"
+	murid[4] = "fajar,"
+	murid[5] = "rio,"
+	murid[6] = "riko,"
+	murid[7] = "dani,"
+	murid[8] = "alfi"
+	var allMurid = murid
+	fmt.Println(allMurid)
+}
