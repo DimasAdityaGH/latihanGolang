25df3957cac4ecfac306124f80692ecf5fbc8f52
dimasAditya <dimasubuntu21@gmail.com>
2021-07-15 14:24:59 +0700

    typedeclaration

---
+++ latihan15jul/typedec.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	type str string
+	type numb int8 
+
+	var pembeli str = "siti"
+	var idpembeli int8 = 12
+
+	fmt.Println(pembeli)
+	fmt.Println(idpembeli)
+}
