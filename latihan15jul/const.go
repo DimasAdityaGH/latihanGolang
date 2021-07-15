7440fb0afd6658f9f6646e577e08647af8d91e8c
dimasAditya <dimasubuntu21@gmail.com>
2021-07-15 13:54:39 +0700

    const

---
+++ latihan15jul/const.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main () {
+	const idDokter int = 120210//const adalah nilai tetap
+	const IdPasien int = 121212//const tidak bisa diubah nilai nya
+	fmt.Println(idDokter)
+	fmt.Println(IdPasien)
+}
