3a55fa16fffd2515d41e817ae5d95f166b597cd4
dimasAditya <dimasubuntu21@gmail.com>
2021-07-15 14:01:09 +0700

    variabel

---
+++ latihan15jul/variabel.go
@@ -0,0 +1,12 @@
+package main 
+
+import "fmt"
+
+func main() {
+	var namaMurid = "dian"// variabel nilai data nya bisa dirubah
+	var nomorMurid = 10
+	nomorMurid = 12
+
+	fmt.Println(namaMurid)
+	fmt.Println(nomorMurid)
+}
