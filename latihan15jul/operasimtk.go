d5f419308168a19a84009f6ab39143ee7a597049
dimasAditya <dimasubuntu21@gmail.com>
2021-07-15 14:21:32 +0700

    operasimtk

---
+++ latihan15jul/operasimtk.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 10
+	var b = 20
+	var c = a + b
+	fmt.Println(c)
+
+	var i = 10
+	i++
+	fmt.Println(i)
+
+	var p = 20
+	p +=20
+	fmt.Println(p)
+
+	var result = 20 + 21
+	fmt.Println(result)
+}
