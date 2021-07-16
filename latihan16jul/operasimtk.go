09518913e0ed5e3f47f136ae64b261a69f1e636d
dimasAditya <dimasubuntu21@gmail.com>
2021-07-16 19:17:30 +0700

    operasi matematika

---
+++ latihan16jul/operasimtk.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 10
+	var b = 20
+	var c = a + b
+	fmt.Println(c)
+
+	var result = 10 + 1
+	fmt.Println(result)
+
+	var i = 10
+	i++
+	fmt.Println(i)
+
+	var o = 10
+	o += 10
+	fmt.Println(o)
+}
