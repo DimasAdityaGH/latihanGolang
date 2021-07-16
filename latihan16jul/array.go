99d67f55137e4a58cef70fde812619aec7bdc0b5
dimasAditya <dimasubuntu21@gmail.com>
2021-07-16 19:36:37 +0700

    array

---
+++ latihan16jul/array.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nilai = [03]int8 {
+		80,
+		90,
+		80,
+	}
+	fmt.Println(nilai)
+
+	var murid02 [1]string
+	murid02[0] = "fahri"
+	fmt.Println(murid02)
+	
+}
