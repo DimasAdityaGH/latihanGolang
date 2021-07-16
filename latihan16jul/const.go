dc821b8049ff97627cf0c2ffdd281e0a5a8dbb42
dimasAditya <dimasubuntu21@gmail.com>
2021-07-16 19:24:32 +0700

    constant

---
+++ latihan16jul/const.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	const ( //const nilai nya tetap 
+		produk = "susu"
+		harga = 10000
+	)
+	fmt.Println("produk:", produk)
+	fmt.Println( "harga:", harga)
+}
