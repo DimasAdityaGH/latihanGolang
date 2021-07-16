a09669cb84fa5631b05bdb5d6f0bb54a84241a9f
dimasAditya <dimasubuntu21@gmail.com>
2021-07-16 19:28:13 +0700

    perbandingan

---
+++ latihan16jul/perbandingan.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var dimas = 80
+	var fifi = 100
+	fmt.Println(dimas == fifi)
+
+	var gula = "manis"
+	var gorengan = "gurih"
+	var perbedaan = gula == gorengan
+	fmt.Println(perbedaan)
+}
