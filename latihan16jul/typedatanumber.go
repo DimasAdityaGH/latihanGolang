8a70ff9f9737d0868ae1c4b2a31d5bf4052a4dfe
dimasAditya <dimasubuntu21@gmail.com>
2021-07-16 19:30:58 +0700

    type data number

---
+++ latihan16jul/typedatanumber.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nilai8 int8 = 127
+	var nilai16 int16 = 32767
+	var nilai32 int32 = 327698
+	var nilai64 int64 = 12120039993
+	fmt.Println(nilai8)
+	fmt.Println(nilai16)
+	fmt.Println(nilai32)
+	fmt.Println(nilai64)
+}
