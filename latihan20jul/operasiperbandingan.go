5e41f3be842dbb2407ed65326f51e0fb5e6091f9
dimasAditya <dimasubuntu21@gmail.com>
2021-07-20 13:04:11 +0700

    operasi perbandingan

---
+++ latihan20jul/operasiperbandingan.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main () {
+	dimas := "boys"
+	fifi := "girls"
+	fmt.Println(dimas == fifi)// false
+
+	var nilai1 = 10
+	var nilai2 = 10
+	var hasil = nilai1 == nilai2
+	fmt.Println(hasil) //true
+}
