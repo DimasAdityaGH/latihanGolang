dad118735654705e2e372001476198cb92d93df4
dimasAditya <dimasubuntu21@gmail.com>
2021-07-20 13:01:25 +0700

    operasi matematika

---
+++ latihan20jul/operasiMath.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main () {
+	//operasi matematika
+	var a = 30
+	var b = 20
+	fmt.Println(a + b)
+	fmt.Println(a - b)
+	fmt.Println(a * b)
+	fmt.Println(a / b)
+
+	var i = 10
+	i++
+	fmt.Println(i)
+
+	var n = 10
+	n += 10
+	fmt.Println(n)
+
+	var hasil = 20 / 2 
+	fmt.Println(hasil)
+}
