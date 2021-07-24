015090f00c503696042fde79ec5c9af343945be3
dimasAditya <dimasubuntu21@gmail.com>
2021-07-24 13:30:52 +0700

    array & operasi matematika

---
+++ latihan24jul/array.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main () {
+	months := []string {"januari", "februari", "maret", "april", "mei", "juni", "juli", "agustus", "september", "oktober", "november", "desember"}
+	fmt.Println(months)
+}
