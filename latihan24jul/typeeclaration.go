f3f378202dda59a538be1463095b015a26898785
dimasAditya <dimasubuntu21@gmail.com>
2021-07-24 13:18:57 +0700

    type declaration

---
+++ latihan24jul/typeeclaration.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main () {
+	type str string
+	type numb int
+
+	var nama str = "Esa dimas aditya"
+	var ktp numb = 12131212
+
+	fmt.Println(nama)
+	fmt.Println(ktp)
+}
