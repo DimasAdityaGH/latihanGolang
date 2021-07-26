e7e27b80b4b665b0e40b21f52194bb63716e4a8a
dimasAditya <dimasubuntu21@gmail.com>
2021-07-26 15:04:47 +0700

    if expression

---
+++ laithan26jul/if.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main () {
+	//if expression
+	//while true
+	// name := "alif"
+
+	// if name == "alif" {
+	// 	fmt.Println("hello, dimas")
+	// } else if name == "alif" {
+	// 	fmt.Println("hello, alif")
+	// }else {
+	// 	fmt.Println("hello world")
+	// }
+
+
+	salam := "world"
+
+	if salam == "world" {
+		fmt.Println("hello world!")
+	} else if salam == "joko" {
+		fmt.Println("hello, joko!")
+	} else if salam == "dono" {
+		fmt.Println("hello, dono!")
+	} else {
+		fmt.Println("error")
+	}
+
+	
+	if length := len(salam);length > 5 {
+		fmt.Println("nama terlalu panjang")
+	} else {
+		fmt.Println("nama sudah benar")
+	}
+}
