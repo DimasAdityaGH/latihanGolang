a8591fecf2d5108c2176e6486f40c4d103203dbd
dimasAditya <dimasubuntu21@gmail.com>
2021-07-26 15:12:07 +0700

    if

--- laithan26jul/if.go
+++ laithan26jul/if.go
@@ -1,37 +1,37 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main () {
-	//if expression
-	//while true
-	// name := "alif"
+// func main () {
+// 	//if expression
+// 	//while true
+// 	// name := "alif"
 
-	// if name == "alif" {
-	// 	fmt.Println("hello, dimas")
-	// } else if name == "alif" {
-	// 	fmt.Println("hello, alif")
-	// }else {
-	// 	fmt.Println("hello world")
-	// }
+// 	// if name == "alif" {
+// 	// 	fmt.Println("hello, dimas")
+// 	// } else if name == "alif" {
+// 	// 	fmt.Println("hello, alif")
+// 	// }else {
+// 	// 	fmt.Println("hello world")
+// 	// }
 
 
-	salam := "world"
+// 	salam := "world"
 
-	if salam == "world" {
-		fmt.Println("hello world!")
-	} else if salam == "joko" {
-		fmt.Println("hello, joko!")
-	} else if salam == "dono" {
-		fmt.Println("hello, dono!")
-	} else {
-		fmt.Println("error")
-	}
+// 	if salam == "world" {
+// 		fmt.Println("hello world!")
+// 	} else if salam == "joko" {
+// 		fmt.Println("hello, joko!")
+// 	} else if salam == "dono" {
+// 		fmt.Println("hello, dono!")
+// 	} else {
+// 		fmt.Println("error")
+// 	}
 
 	
-	if length := len(salam);length > 5 {
-		fmt.Println("nama terlalu panjang")
-	} else {
-		fmt.Println("nama sudah benar")
-	}
-}
+// 	if length := len(salam);length > 5 {
+// 		fmt.Println("nama terlalu panjang")
+// 	} else {
+// 		fmt.Println("nama sudah benar")
+// 	}
+// }
