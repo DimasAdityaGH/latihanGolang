ee9e15f508231b742824e4da36122a5030952360
dimasAditya <dimasubuntu21@gmail.com>
2021-07-28 12:11:16 +0700

    switch

---
+++ latihan28jul/switch.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main () {
+	var name = "dimass"
+
+	switch name {
+	case "dimas":
+		fmt.Println("hello dimas")
+	case "farhan":
+		fmt.Println("hello farhan")
+	default:
+		fmt.Println("hello world!")
+	}
+
+	// switch length := len(name); length > 5 {
+	// case true:
+	// 	fmt.Println("nama terlalu panjang")
+	// case false:
+	// 	fmt.Println("nama sudah benar")
+	// }
+
+	length := len(name)
+	switch {
+	case length > 10:
+		fmt.Println("nama terlalu panjang")
+	case length > 5:
+		fmt.Println("nama lumayan panjang")
+	default:
+		fmt.Println("nama sudah cukup")
+	}
+}
