f9cc01c3c82e2350ea58c88e85fc716b78b8c906
dimasAditya <dimasubuntu21@gmail.com>
2021-07-30 15:46:32 +0700

    array

---
+++ latihan30jul/array.go
@@ -0,0 +1,22 @@
+package main 
+
+import "fmt" 
+
+func main () {
+	var workDay = [5]string {
+		"senin",
+		"selasa",
+		"rabu",
+		"kamis",
+		"juma5",
+	}
+
+	fmt.Println(workDay)
+
+	var libur [2]string
+	libur[0] = "sabtu"
+	libur[1] = "minggu"
+	fmt.Println(libur)
+	fmt.Println(len(libur))
+	fmt.Println(cap(libur))
+}
