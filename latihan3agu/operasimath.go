b924bfed511cc01c956bf7d0d10ad9c253cf49f3
dimasAditya <dimasubuntu21@gmail.com>
2021-08-03 14:23:03 +0700

    operasi matematika

---
+++ latihan3agu/operasimath.go
@@ -0,0 +1,31 @@
+package main 
+
+import "fmt" 
+
+func main () {
+	var a = 10
+	var b = 20
+	var c = a * b
+	fmt.Println(c)
+
+	i := 10
+	i++
+	fmt.Println(i)
+
+	var l = 10
+	l += 10
+	fmt.Println(l)
+
+	o := 1
+	o -= 0
+	fmt.Println(o)
+
+	fmt.Println(1 +1)
+
+	const k = 10
+	const p = 20
+	
+	const result = k + p
+	fmt.Println(result)
+
+}
