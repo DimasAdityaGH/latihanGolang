f230b68a609981458d02788594fc53c03874a7f5
dimasAditya <dimasubuntu21@gmail.com>
2021-08-03 14:16:59 +0700

    perbandingan

---
+++ latihan3agu/perbandingan.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt" 	
+)
+
+
+func main () {
+	var a = 10
+	var b = 11
+	var c = a > b
+	fmt.Println(c)
+
+	var dimas = "laki laki"
+	var fifi = "perempuan"
+	fmt.Println(dimas == fifi)
+}
