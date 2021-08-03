e51e8398f9eb0b0887a6f3e73d02d50f0d8f0081
dimasAditya <dimasubuntu21@gmail.com>
2021-08-03 14:07:09 +0700

    constant

---
+++ latihan3agu/const.go
@@ -0,0 +1,13 @@
+package main 
+
+import "fmt" 
+
+func main () {
+	const nama = "dimas"
+	fmt.Println(nama)
+	const (
+		firstName = "dimas"
+		lastName = "aditya"
+	)
+	fmt.Println(firstName, lastName)
+}
